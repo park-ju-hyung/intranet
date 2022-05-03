3444898165e7a177c9c75e5095d53319a209448d
ptg0131 <ptg0131@gmail.com>
2022-05-03 09:48:52 +0900

    java css source 추가

---
+++ src/main/resources/static/res/site/js/mobile_menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	var windowWidth = $(window).width();
+	if(windowWidth < 1000){
+		$(".standard_left").slideUp();
+
+		$(".menu > li > a").attr("href","javscript:;");
+		$(".menu > li > div").hide();
+		$(".menu > li > a").click(function(){
+			var subcont = $(this).next("div");
+
+			if( subcont.is(":visible") ){ //닫을 때
+				subcont.slideUp();
+			}else{
+				subcont.slideDown(); // 열릴 때						 
+				$(".menu > li > div").not(subcont).slideUp();
+			}         
+		});
+	} 
+});
