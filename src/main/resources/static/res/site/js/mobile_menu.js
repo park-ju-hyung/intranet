b030f844c3bfc7852c23c45ca910dc564dceaeac
ptg0131 <ptg0131@gmail.com>
2022-05-04 09:09:47 +0900

    작업중 220504

--- src/main/resources/static/res/site/js/mobile_menu.js
+++ src/main/resources/static/res/site/js/mobile_menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 	var windowWidth = $(window).width();
 	if(windowWidth < 1000){
@@ -17,24 +16,4 @@ $(document).ready(function(){
 			}         
 		});
 	} 
-=======
-$(document).ready(function(){
-	var windowWidth = $(window).width();
-	if(windowWidth < 1000){
-		$(".standard_left").slideUp();
-
-		$(".menu > li > a").attr("href","javscript:;");
-		$(".menu > li > div").hide();
-		$(".menu > li > a").click(function(){
-			var subcont = $(this).next("div");
-
-			if( subcont.is(":visible") ){ //닫을 때
-				subcont.slideUp();
-			}else{
-				subcont.slideDown(); // 열릴 때						 
-				$(".menu > li > div").not(subcont).slideUp();
-			}         
-		});
-	} 
->>>>>>> branch 'master' of https://github.com/inikorea/ini-home2022.git
 });
--- src/main/resources/static/res/site/js/xmlHttpRequest.js
+++ src/main/resources/static/res/site/js/xmlHttpRequest.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function getXMLHttpRequest(){
         if(window.ActiveXObject){
               try{
@@ -15,22 +14,4 @@ function getXMLHttpRequest(){
         }else{
               return null;
         }
-=======
-function getXMLHttpRequest(){
-        if(window.ActiveXObject){
-              try{
-                    return new ActiveXObject("Msxml2.XMLHTTP");
-              }catch(e){
-                     try{
-                           return new ActiveXObject("Microsoft.XML");
-                     }catch(e1){
-                            return null;
-                     }
-              }    
-        }else if(window.XMLHttpRequest){
-              return new XMLHttpRequest();
-        }else{
-              return null;
-        }
->>>>>>> branch 'master' of https://github.com/inikorea/ini-home2022.git
  }
