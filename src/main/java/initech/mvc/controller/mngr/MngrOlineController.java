27801a380f1d9a0f735ddecd65acb5c2768618af
benchel <whalswl1030@nate.com>
2022-05-16 17:04:36 +0900

    관리자 로그인 시 바로 온라인문의 목록으로 오도록 변경

--- src/main/java/initech/mvc/controller/mngr/MngrOlineController.java
+++ src/main/java/initech/mvc/controller/mngr/MngrOlineController.java
@@ -17,6 +17,7 @@ import initech.mvc.dto.OnlineDTO;
 import initech.mvc.service.mngr.MngrOlineService;
 
 @Controller
+@RequestMapping("/mngr/*")
 public class MngrOlineController {
 	
 	@Autowired
@@ -24,7 +25,7 @@ public class MngrOlineController {
 	
 
 	// 관리자 > 게시판 > 리스트 페이지
-    @RequestMapping(value = {"/mngr/withUs/online-list"}, method = {RequestMethod.GET})
+    @RequestMapping(value = {"", "/withUs/online-list"}, method = {RequestMethod.GET})
     public String mngrList(
     		@ModelAttribute OnlineDTO onlineDTO
             , ModelMap modelMap
@@ -35,14 +36,14 @@ public class MngrOlineController {
 
      //관리자 > 게시판 > 데이터 전체 조회
     @ResponseBody
-    @RequestMapping(value = {"/mngr/online/find-all"}, method = {RequestMethod.POST})
+    @RequestMapping(value = {"/online/find-all"}, method = {RequestMethod.POST})
     public ResponseEntity<?> mngrFindAll(@RequestBody OnlineDTO onlineDTO) throws Exception {
         return new ResponseEntity<>(mngrOlineService.mngrFindAll(onlineDTO), HttpStatus.OK);
     }
     
     
     // 관리자 > 게시판 > view 페이지
-	@GetMapping({"/mngr/withUs/online-view"})
+	@GetMapping({"/withUs/online-view"})
 	public String mngrView(ModelMap modelMap) throws Exception {
 		return "mngr/withUs/online-view";
 	}
@@ -50,7 +51,7 @@ public class MngrOlineController {
 	
     // 관리자 > 게시판 > 데이터 조회
     @ResponseBody
-    @RequestMapping(value = {"/mngr/online/find"}, method = {RequestMethod.POST})
+    @RequestMapping(value = {"/online/find"}, method = {RequestMethod.POST})
     public ResponseEntity<?> mngrFind(@RequestBody OnlineDTO onlineDTO
             , BindingResult bindingResult
             ) throws Exception {
@@ -60,7 +61,7 @@ public class MngrOlineController {
     
     // 관리자 > 게시판 > 제거
     @ResponseBody
-    @RequestMapping(value = {"/mngr/online/remove"}, method = {RequestMethod.POST})
+    @RequestMapping(value = {"/online/remove"}, method = {RequestMethod.POST})
     public Object mngrRemove(
             @RequestBody OnlineDTO onlineDTO
             ) throws Exception {
