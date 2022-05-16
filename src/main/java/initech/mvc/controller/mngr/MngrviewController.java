23ccb276bcc47ea04c06221008fb8ec240c6b338
benchel <whalswl1030@nate.com>
2022-05-16 17:04:49 +0900

    불필요한 import 제거

--- src/main/java/initech/mvc/controller/mngr/MngrviewController.java
+++ src/main/java/initech/mvc/controller/mngr/MngrviewController.java
@@ -1,15 +1,9 @@
 package initech.mvc.controller.mngr;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import initech.mvc.dto.MngrDTO;
 import lombok.RequiredArgsConstructor;
 
 @Controller
