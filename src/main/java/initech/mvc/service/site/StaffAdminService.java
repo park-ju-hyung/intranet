ac4d630b1be0325a663bb285ea13030af32b54eb
park-ju-hyung <ptg0131@gmail.com>
2024-08-19 21:18:04 +0900

    초기화

--- src/main/java/initech/mvc/service/site/StaffAdminService.java
+++ src/main/java/initech/mvc/service/site/StaffAdminService.java
@@ -19,64 +19,6 @@ public class StaffAdminService {
         this.staffMapper = staffMapper;
     }
 
-    // 관리자 > 회원관리 > 검색조건
-    public List<StaffVO> searchusers(int page, int size, String memberName, LocalDate searchStartDate, LocalDate searchEndDate) {
-        int offset = (page - 1) * size;
-        return staffMapper.searchusers(offset, size, memberName, searchStartDate, searchEndDate);
-    }
-
-    // 관리자 > 회원관리 > 검색조건 > 검색된 사용자의 총 개수를 반환하는 메서드
-    public int getfilteredusercount(String memberName, LocalDate searchStartDate, LocalDate searchEndDate) {
-        return staffMapper.getfilteredusercount(memberName, searchStartDate, searchEndDate);
-    }
-
-    // 관리자 > 회원관리 > view 수정 기능
-    public void updateStaff(Long regId, StaffVO staff) {
-        staffMapper.updatestaff(regId, staff);
-    }
-
-
-
-
-
-
-    // 관리자 > 회원가입승인 > 검색조건
-    public List<StaffVO> searchpermission(int page, int size, String memberName, String memberPermission, LocalDate searchStartDate, LocalDate searchEndDate) {
-        int offset = (page - 1) * size;
-        return staffMapper.searchpermission(offset, size, memberName, memberPermission, searchStartDate, searchEndDate);
-    }
-
-    // 관리자 > 회원관리 > 검색조건 > 검색된 사용자의 총 개수를 반환하는 메서드
-    public int getfilteredpermissioncount(String memberName, String memberPermission, LocalDate searchStartDate, LocalDate searchEndDate) {
-        return staffMapper.getfilteredpermissioncount(memberName, memberPermission, searchStartDate, searchEndDate);
-    }
-
-    // 관리자 > 회원가입승인 > view 수정 기능
-    public int updatepermissionstaff(Long regId, StaffVO staff) {
-        return staffMapper.updatepermissionstaff(regId, staff);
-    }
-
-
-
-
-
-
-
-    // 페이징 처리를 위한 메서드
-    public List<StaffVO> getUsersByPage(int page, int size) {
-        int offset = (page - 1) * size;
-        return staffMapper.selectallusers(offset, size);
-    }
-
-    public int getTotalUserCount() {
-        return staffMapper.countallusers();
-    }
-
-    // 상세조회
-    public StaffVO UsersDetail(Long regId) {
-        return staffMapper.usersdetail(regId);
-    }
-
 
 
 
--- src/main/java/initech/mvc/service/site/StaffService.java
+++ src/main/java/initech/mvc/service/site/StaffService.java
@@ -11,52 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StaffService {
-    private final StaffMapper staffMapper;
-
-    @Autowired
-    public StaffService(StaffMapper staffMapper) {
-        this.staffMapper = staffMapper;
-    }
-
-    @Transactional
-    public void register(StaffVO staff) {
-        staffMapper.insertstaff(staff);
-    }
-
-    // login
-    public StaffVO login(String email, String password) {
-        return staffMapper.findbyemailandpassword(email, password);
-    }
-
-    // 이메일 중복 확인 메서드
-    public StaffVO findbyemail(String email) {
-        return staffMapper.findbyemail(email);
-    }
-
-    // 아이디찾기
-    public StaffVO findbyid(String name, String birth) {
-        return staffMapper.findbyid(name, birth);
-    }
-
-    // 비밀번호 찾기
-    public StaffVO findbypassword(String email, String name, String birth) {
-        return staffMapper.findbypassword(email, name, birth);
-    }
-
-    // 비밀번호 재설정 - 경로에 id 추가
-    public StaffVO usernewpassword(Long regId) {
-        return staffMapper.usernewpassword(regId);
-    }
-
-    // 비밀번호 재설정 - 기능
-    public void reginewpassword(Long regId, RegiPasswordDTO regiPasswordDTO) {
-        staffMapper.reginewpassword(regId, regiPasswordDTO);
-    }
-
-
-
-
-
 
 
 
